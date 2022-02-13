9151e58b416710621fd5a8040be939286d291711
luoxding <anthony_lambert@live.com>
2022-02-14 03:48:15 +0800

    2022-02-14

---
+++ push.sh
@@ -0,0 +1,7 @@
+now=$(date "+%Y-%m-%d")
+# echo "Change Directory to D:/Code"
+# cd D:/Code
+echo "Starting add-commit-pull-push..."
+git add -A && git commit -m "$now" && git pull && git push
+echo "Finish!"
+read
